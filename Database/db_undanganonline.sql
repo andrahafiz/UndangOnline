ad1016d5bf84ce1fa7ca02a7f892c719740f7dbb
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-05-05 16:00:50 +0700

    Update db_undanganonline.sql

--- Database/db_undanganonline.sql
+++ Database/db_undanganonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Apr 2021 pada 16.12
+-- Waktu pembuatan: 05 Bulan Mei 2021 pada 11.00
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 8.0.3
 
@@ -46,10 +46,20 @@ CREATE TABLE `tb_acara` (
 --
 
 INSERT INTO `tb_acara` (`ID_Acara`, `TglAkad`, `WaktuMulaiAkad`, `WaktuSelesaiAkad`, `TempatAkad`, `AlamatAkad`, `TglResepsi`, `WaktuMulaiResepsi`, `WaktuSelesaiResepsi`, `TempatResepsi`, `AlamatResepsi`) VALUES
-('ACR1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('ACR2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('ACR3', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('ACR4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+('ACR1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `ID_Admin` varchar(256) NOT NULL,
+  `Username_Admin` varchar(100) NOT NULL,
+  `Password_Admin` varchar(256) NOT NULL,
+  `Status_Admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -72,7 +82,7 @@ CREATE TABLE `tb_akun` (
 --
 
 INSERT INTO `tb_akun` (`ID_akun`, `Username`, `Email_akun`, `NoHp_akun`, `Password_akun`, `Created_akun`, `Status_akun`) VALUES
-('AKN1', 'andra', 'andra18ti@mahasiswa.pcr.ac.id', '0822', '$2y$10$9COPhnTe2M8bZ19bQING4uUcqgYVmINlhCQcTuBsp2a3WvwaMKvHK', 1618569708, '1');
+('AKN1', 'Andra Hafiz', 'asd@gmail', '082276853382', '$2y$10$EjkyNFYFZKtPiEhlU0nKJ.Pc.y.i/Df4z0UUNs4dO.XpUAf6KvlPy', 1618680851, '1');
 
 -- --------------------------------------------------------
 
@@ -137,10 +147,7 @@ CREATE TABLE `tb_mempelai` (
 --
 
 INSERT INTO `tb_mempelai` (`ID_Mempelai`, `Nama_MPria`, `Nama_MWanita`, `NoHp_MPria`, `NoHp_MWanita`, `Alamat_MPria`, `Alamat_MWanita`, `Panggilan_MPria`, `Panggilan_MWanita`, `NamaOrtu_Ayah_MPria`, `NamaOrtu_Ibu_MPria`, `NamaOrtu_Ayah_MWanita`, `NamaOrtu_Ibu_MWanita`, `Foto_MPria`, `Foto_MWanita`) VALUES
-('MPL1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('MPL2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('MPL3', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('MPL4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+('MPL1', 'asd', 'asd', '123', '123', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'foto_pria.jpg', 'foto_cewek.jpg');
 
 -- --------------------------------------------------------
 
@@ -184,9 +191,9 @@ CREATE TABLE `tb_tamu` (
   `ID_TamuUndangan` varchar(256) NOT NULL,
   `ID_Undangan` varchar(256) NOT NULL,
   `Nama_Tamu` varchar(150) NOT NULL,
-  `Wa_Tamu` varchar(12) NOT NULL,
-  `Email_Tamu` varchar(100) NOT NULL,
-  `UcapanSelamat` text NOT NULL
+  `Wa_Tamu` varchar(15) DEFAULT NULL,
+  `Email_Tamu` varchar(100) DEFAULT NULL,
+  `UcapanSelamat` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -194,7 +201,7 @@ CREATE TABLE `tb_tamu` (
 --
 
 INSERT INTO `tb_tamu` (`ID_TamuUndangan`, `ID_Undangan`, `Nama_Tamu`, `Wa_Tamu`, `Email_Tamu`, `UcapanSelamat`) VALUES
-('TMU1', 'UND1', 'Andra Hafiz', '082276853382', 'andra18ti@mahasiswa.pcr.ac.id', 'Selamat ya');
+('TMU1', 'UND1', 'Kom', '123', 'kom@gmail.com', 'kosong');
 
 -- --------------------------------------------------------
 
@@ -218,13 +225,7 @@ CREATE TABLE `tb_undangan` (
 --
 
 INSERT INTO `tb_undangan` (`ID_Undangan`, `ID_Mempelai`, `ID_Acara`, `ID_Kategori`, `ID_Tema`, `ID_Akun`, `tgl_buatakun`, `tgl_selesaiakun`) VALUES
-('UND1', 'MPL1', 'ACR1', 'KTG1', 'THM1', 'AKN1', 1618553945, 1619158745),
-('UND2', 'MPL2', 'ACR2', 'KTG1', 'THM1', 'AKN2', 1618553976, 1619158776),
-('UND3', 'MPL3', 'ACR3', 'KTG1', 'THM1', 'AKN3', 1618567787, 1619172587),
-('UND4', 'MPL1', 'ACR1', 'KTG1', 'THM1', 'AKN1', 1618568894, 1619173694),
-('UND5', 'MPL2', 'ACR2', 'KTG1', 'THM1', 'AKN1', 1618569028, 1619173828),
-('UND6', 'MPL3', 'ACR3', 'KTG1', 'THM1', 'AKN1', 1618569392, 1619174192),
-('UND7', 'MPL4', 'ACR4', 'KTG1', 'THM1', 'AKN1', 1618569708, 1619174508);
+('UND1', 'MPL1', 'ACR1', 'KTG1', 'THM1', 'AKN1', 1618680851, 1619285651);
 
 -- --------------------------------------------------------
 
@@ -262,23 +263,6 @@ CREATE TABLE `token` (
   `token` varchar(128) NOT NULL,
   `date_created` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `token`
---
-
-INSERT INTO `token` (`idtoken`, `email`, `token`, `date_created`) VALUES
-(10, 'andra18ti@mahasiswa.pcr.ac.id', 'GPu2qfFg28naJGWt2zDKIN6IAElqNzgrGHTs56PI3XA=', 1618306168),
-(11, 'andra18ti@mahasiswa.pcr.ac.id', 'hAu8pYglsD8zj7OKBG5zR+nOAkor9xLbdYZWWLgrU2U=', 1618306241),
-(12, 'andra18ti@mahasiswa.pcr.ac.id', 'hBdzxce3vfoMhO/Q72wso3/P58u2fpmMoNIzAvJ9ew8=', 1618306264),
-(13, 'andra18ti@mahasiswa.pcr.ac.id', 'ZVJGUGEvTeyi9Ao/VDdvuBZV2yrUGSEjNtTt99drc20=', 1618306344),
-(14, 'andra18ti@mahasiswa.pcr.ac.id', 'jgh9cjeA72w2VYsCU+960QXDiR4FXaJKXMgOaw5Kp2o=', 1618306634),
-(15, 'andra18ti@mahasiswa.pcr.ac.id', 'UACZXCtAXJRhSngMJO/sGRAP9ZmdGT6AIjrVYZqzT7U=', 1618306778),
-(16, 'andra18ti@mahasiswa.pcr.ac.id', 'UPKoBo6Nv9Og9yxk3ykDnJRIZTEq/Sq3Y0X8Z2ulctU=', 1618306835),
-(17, 'andra18ti@mahasiswa.pcr.ac.id', 'Iv2UG9q0JKd5IpBhVDTuWm1e2i3TatjfBnuEI31UdtA=', 1618307024),
-(18, 'andra18ti@mahasiswa.pcr.ac.id', 'JM0ZhBUxVTwppsYhrpwBK2pM8jXG6roT3JfHfbuq+AI=', 1618307056),
-(19, 'andra18ti@mahasiswa.pcr.ac.id', 'h/l+uqXiQBRfzgbc0W11cdaamHrEP62TGcFyB1GerCo=', 1618307162),
-(20, 'andra18ti@mahasiswa.pcr.ac.id', 'Ui+eOyu8yLblAY8K+RNYpd7o1039Sy3roCSxnk4nHjw=', 1618307375);
 
 -- --------------------------------------------------------
 
@@ -408,6 +392,12 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 ALTER TABLE `tb_acara`
   ADD PRIMARY KEY (`ID_Acara`),
   ADD KEY `ID_Acara` (`ID_Acara`);
+
+--
+-- Indeks untuk tabel `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`ID_Admin`);
 
 --
 -- Indeks untuk tabel `tb_akun`
