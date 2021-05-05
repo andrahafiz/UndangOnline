a6c4909015412c00f56a858a00aa75da25c66ba5
nanhal <andrey18ti@mahasiswa.pcr.ac.id>
2021-05-05 16:21:53 +0700

    Update V_akun.php

--- application/views/Admin/Akun/V_akun.php
+++ application/views/Admin/Akun/V_akun.php
@@ -4,9 +4,7 @@
         <div class="main-body">
             <div class="page-wrapper">
                 <!-- Page-header start -->
-
                 <!-- Page-header end -->
-
                 <!-- Page-body start -->
                 <div class="page-body">
                     <div class="row">
@@ -19,8 +17,6 @@
                                 <div class="card-block">
                                     <div class="dt-responsive table-responsive">
                                         <div id="basic-btn_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                                            <div class="dt-buttons"><a class="dt-button buttons-copy buttons-html5" tabindex="0" aria-controls="basic-btn" href="#"><span>Copy</span></a><a class="dt-button buttons-csv buttons-html5" tabindex="0" aria-controls="basic-btn" href="#"><span>CSV</span></a><a class="dt-button buttons-excel buttons-html5" tabindex="0" aria-controls="basic-btn" href="#"><span>Excel</span></a><a class="dt-button buttons-pdf buttons-html5" tabindex="0" aria-controls="basic-btn" href="#"><span>PDF</span></a><a class="dt-button buttons-print" tabindex="0" aria-controls="basic-btn" href="#"><span>Print</span></a></div>
-                                            <div id="basic-btn_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="basic-btn"></label></div>
                                             <table id="basic-btn" class="table table-striped table-bordered nowrap dataTable" role="grid" aria-describedby="basic-btn_info">
                                                 <thead>
                                                     <tr role="row">
@@ -35,9 +31,6 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-
-
-
                                                     <tr role="row" class="odd">
                                                         <td class="sorting_1">Airi Satou</td>
                                                         <td>Accountant</td>
@@ -45,78 +38,12 @@
                                                         <td>33</td>
                                                         <td>2008/11/28</td>
                                                         <td>$162,700</td>
-                                                    </tr>
-                                                    <tr role="row" class="even">
-                                                        <td class="sorting_1">Ashton Cox</td>
-                                                        <td>Junior Technical Author</td>
-                                                        <td>San Francisco</td>
-                                                        <td>66</td>
-                                                        <td>2009/01/12</td>
-                                                        <td>$86,000</td>
-                                                    </tr>
-                                                    <tr role="row" class="odd">
-                                                        <td class="sorting_1">Bradley Greer</td>
-                                                        <td>Software Engineer</td>
-                                                        <td>London</td>
-                                                        <td>41</td>
-                                                        <td>2012/10/13</td>
-                                                        <td>$132,000</td>
-                                                    </tr>
-                                                    <tr role="row" class="even">
-                                                        <td class="sorting_1">Brielle Williamson</td>
-                                                        <td>Integration Specialist</td>
-                                                        <td>New York</td>
-                                                        <td>61</td>
-                                                        <td>2012/12/02</td>
-                                                        <td>$372,000</td>
-                                                    </tr>
-                                                    <tr role="row" class="odd">
-                                                        <td class="sorting_1">Cedric Kelly</td>
-                                                        <td>Senior Javascript Developer</td>
-                                                        <td>Edinburgh</td>
-                                                        <td>22</td>
-                                                        <td>2012/03/29</td>
-                                                        <td>$433,060</td>
-                                                    </tr>
-                                                    <tr role="row" class="even">
-                                                        <td class="sorting_1">Charde Marshall</td>
-                                                        <td>Regional Director</td>
-                                                        <td>San Francisco</td>
-                                                        <td>36</td>
-                                                        <td>2008/10/16</td>
-                                                        <td>$470,600</td>
-                                                    </tr>
-                                                    <tr role="row" class="odd">
-                                                        <td class="sorting_1">Colleen Hurst</td>
-                                                        <td>Javascript Developer</td>
-                                                        <td>San Francisco</td>
-                                                        <td>39</td>
-                                                        <td>2009/09/15</td>
-                                                        <td>$205,500</td>
-                                                    </tr>
-                                                    <tr role="row" class="even">
-                                                        <td class="sorting_1">Dai Rios</td>
-                                                        <td>Personnel Lead</td>
-                                                        <td>Edinburgh</td>
-                                                        <td>35</td>
-                                                        <td>2012/09/26</td>
-                                                        <td>$217,500</td>
-                                                    </tr>
-                                                    <tr role="row" class="odd">
-                                                        <td class="sorting_1">Garrett Winters</td>
-                                                        <td>Accountant</td>
-                                                        <td>Tokyo</td>
-                                                        <td>63</td>
-                                                        <td>2011/07/25</td>
-                                                        <td>$170,750</td>
-                                                    </tr>
-                                                    <tr role="row" class="even">
-                                                        <td class="sorting_1">Gloria Little</td>
-                                                        <td>Systems Administrator</td>
-                                                        <td>New York</td>
-                                                        <td>59</td>
-                                                        <td>2009/04/10</td>
-                                                        <td>$237,500</td>
+                                                        <td>$162,700</td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-danger  btn-icon waves-effect waves-light disabled" style="height: 30px; width: 30px;" data-toggle="tooltip" data-placement="bottom" title="Hapus">
+                                                                <i class="fa fa-trash"></i>
+                                                            </button>
+                                                        </td>
                                                     </tr>
                                                 </tbody>
                                                 <tfoot>
@@ -129,31 +56,16 @@
                                                         <th rowspan="1" colspan="1">Cretaed Akun</th>
                                                         <th rowspan="1" colspan="1">Status Akun</th>
                                                         <th rowspan="1" colspan="1">Aksi</th>
-
                                                     </tr>
                                                 </tfoot>
                                             </table>
-                                            <div class="dataTables_info" id="basic-btn_info" role="status" aria-live="polite">Showing 1 to 10 of 20 entries</div>
-                                            <div class="dataTables_paginate paging_simple_numbers" id="basic-btn_paginate">
-                                                <ul class="pagination">
-                                                    <li class="paginate_button page-item previous disabled" id="basic-btn_previous"><a href="#" aria-controls="basic-btn" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
-                                                    <li class="paginate_button page-item active"><a href="#" aria-controls="basic-btn" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-                                                    <li class="paginate_button page-item "><a href="#" aria-controls="basic-btn" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-                                                    <li class="paginate_button page-item next" id="basic-btn_next"><a href="#" aria-controls="basic-btn" data-dt-idx="3" tabindex="0" class="page-link">Next</a></li>
-                                                </ul>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!-- HTML5 Export Buttons end -->
-
-
-
                         </div>
                     </div>
                 </div>
-                <!-- Page-body end -->
             </div>
         </div>
     </div>
