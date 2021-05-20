a524ef407985cadf988d34ae657d7b69d2bf72f3
nanhal <andrey18ti@mahasiswa.pcr.ac.id>
2021-05-20 21:54:03 +0700

    dashboard

--- application/controllers/Admin/Dashboard.php
+++ application/controllers/Admin/Dashboard.php
@@ -7,12 +7,23 @@ class Dashboard extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('Tamu_Model');
         $this->load->helper('my_function_helper');
-        cekaccess();
+        $this->load->model('Tamu_Model');
+        $this->load->model('Akun_Model');
+        cekaccess_admin();
     }
     // }
     public function index()
     {
+        $akun = $this->Akun_Model->selectAll();
+        $data = array(
+            'judul' => 'Dashboard',
+            'menu' => menu_admin(),
+            'data_akun' => $akun,
+            'jumlah_akun' => $this->Akun_Model->jumlahtamu()
+        );
+        $this->load->view('Admin/layout/header', $data);
+        $this->load->view('Admin/Dashboard', $data);
+        $this->load->view('Admin/layout/footer');
     }
 }
